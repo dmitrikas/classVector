455cd3adf086c36d90e5efb5ccff1bb767afec9b
dmitrikas <dmvetrenko@yandex.ru>
2015-05-08 15:39:42 +0300

    Add files to repo

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace classVector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector3d a = new Vector3d(1, 2, 3);
+            Vector3d b = new Vector3d(5, 6, 7);
+
+            Vector3d sum = a + b;
+
+            Console.Write(sum); 
+        }
+    }
+}
---
+++ Vector3d.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace classVector
+{
+    class Vector3d
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+        public double z { get; set; }
+
+        public Vector3d(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public static Vector3d operator +(Vector3d v1, Vector3d v2)
+        {
+            return new Vector3d(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+        }
+
+        public static Vector3d operator +(double k, Vector3d v)
+        {
+            return new Vector3d(v.x + k, v.y + k, v.z + k);
+        }
+
+        public static Vector3d operator -(Vector3d v1, Vector3d v2)
+        {
+            return new Vector3d(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+        }
+
+        public static double operator *(Vector3d v1, Vector3d v2)
+        {
+            return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+        }        
+
+        public static Vector3d operator *(double k, Vector3d v)
+        {
+            return new Vector3d(v.x * k, v.y * k, v.z * k);
+        }
+
+        public static Vector3d operator /(Vector3d v, double k)
+        {
+            return new Vector3d(v.x / k, v.y / k, v.z / k);
+        }
+
+        public static Vector3d operator ^(Vector3d v1, Vector3d v2)
+        {
+            return new Vector3d(v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x);
+        }
+
+        public double Length()
+        {
+            return Math.Sqrt(x * x + y * y + z * z);
+        }
+
+        public void normalize()
+        {
+            double locLength = Length();
+            double inv_length = (1 / locLength);
+
+            x *= inv_length;
+            y *= inv_length;
+            z *= inv_length;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("X={0}, Y={1}, Z={2}", x, y, z);
+        }
+
+        
+
+    }
+}
